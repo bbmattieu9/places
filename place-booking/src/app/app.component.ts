a50a15b642cc8058d25e673bd8b9b75dc19142a9
Matthew Ogunmola <matthew.ogunmola@sterling.ng>
2020-02-24 16:51:45 +0100

    Added fake Auth + Guard

--- place-booking/src/app/app.component.ts
+++ place-booking/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './auth/auth.service';
+import { Route } from '@angular/compiler/src/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +16,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authSrv: AuthService,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -25,5 +30,9 @@ export class AppComponent {
     });
   }
 
-  onLogout() { }
+  onLogout() {
+    this.authSrv.logout();
+    this.router.navigateByUrl('/auth');
+
+   }
 }

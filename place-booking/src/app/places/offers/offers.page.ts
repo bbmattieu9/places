554f578fe58708d9a19116b2ace3bde7dfdc8da2
Matthew Ogunmola <matthew.ogunmola@sterling.ng>
2020-02-26 15:03:52 +0100

    Creating Populating and deleting bookings

--- place-booking/src/app/places/offers/offers.page.ts
+++ place-booking/src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -11,9 +13,11 @@ export class OffersPage implements OnInit {
 
   offers: Place[];
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
-  onEdit(offerId: string) {
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
     console.log('Editing Item with ID :=>', offerId);
   }
 

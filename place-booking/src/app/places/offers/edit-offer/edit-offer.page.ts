a6b27b9730002b808bfbee50e3b0c6d24a4d6bab
Matthew Ogunmola <matthew.ogunmola@sterling.ng>
2020-03-02 11:53:09 +0100

    updating my branch

--- place-booking/src/app/places/offers/edit-offer/edit-offer.page.ts
+++ place-booking/src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
+import { Place } from '../../place.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditOfferPage implements OnInit {
 
-  constructor() { }
+  place: Place;
+  editNewOfferform: FormGroup;
+
+  constructor(private route: ActivatedRoute,
+              private navCtrl: NavController,
+              private placesSrv: PlacesService) { }
+
+  onUpdateOffer() {
+    if (!this.editNewOfferform.valid) {
+      return;
+    }
+   }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        return this.navCtrl.navigateBack('places/tabs/offers');
+      }
+      this.place = this.placesSrv.getPlaceById(paramMap.get('placeId'));
+      this.editNewOfferform = new FormGroup({
+        title: new FormControl(this.place.title, {
+          updateOn: 'blur',
+          validators: [Validators.required]
+        }),
+        description: new FormControl(this.place.description, {
+          updateOn: 'blur',
+          validators: [Validators.required, Validators.maxLength(180)]
+        })
+      });
+    });
   }
+
+
 
 }

dc7344d9ef562225eba28dc20a06cdf2f8c633bd
Matthew Ogunmola <matthew.ogunmola@sterling.ng>
2020-03-17 17:06:29 +0100

    managing state and adding new places

--- place-booking/src/app/places/place.model.ts
+++ place-booking/src/app/places/place.model.ts
@@ -1,13 +1,13 @@
 export class Place {
     constructor(
-        public id: string,
-        public title: string,
-        public description: string,
-        public imageUrl: string,
-        public price: number,
-        public location: string,
-        public availableFrom: Date,
-        public availableTo: Date,
-        public userId: string
+        public id?: string,
+        public title?: string,
+        public description?: string,
+        public imageUrl?: string,
+        public price?: number,
+        public location?: string,
+        public availableFrom?: Date,
+        public availableTo?: Date,
+        public userId?: string
     ) { }
 }
